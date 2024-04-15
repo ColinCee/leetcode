bd12e82905174d60c85cb79e9e19d4ff57da3760
Colin Cheung <11246116+ColinCee@users.noreply.github.com>
2024-04-15 21:27:34 +0100

    ADD p0129

---
+++ src/bin/p0129.rs
@@ -0,0 +1,6 @@
+use leetcode::p0129_sum_root_to_leaf_numbers;
+
+fn main() {
+    // Call functions from the problem module
+    p0129_sum_root_to_leaf_numbers::solve();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod p0129_sum_root_to_leaf_numbers;
 pub mod p0226_invert_binary_tree;
 pub mod p0404_sum_of_left_leaves;
 pub mod trees;
